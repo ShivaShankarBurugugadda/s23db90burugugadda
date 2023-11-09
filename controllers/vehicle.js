426bb9f2f56a401fb2fbb3951a06bab2f0d941a0
ShivaShankarBurugugadda <143209402+ShivaShankarBurugugadda@users.noreply.github.com>
2023-11-09 12:40:09 -0600

    latest commit

--- controllers/vehicle.js
+++ controllers/vehicle.js
@@ -15,16 +15,47 @@ res.send(`{"error": ${err}}`);
 
 
 
+// VIEWS
+// Handle a show all view
+exports.vehicle_view_all_Page = async function(req, res) {
+    try{
+    thevehicle = await vehicle.find();
+    res.render('vehicle', { title: 'vehicle Search Results', results: thevehicle });
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+    };
 
+
+
+
+exports.vehicle_create_post = async function(req, res) {
+console.log(req.body)
+let document = new vehicle();
+// We are looking for a body, since POST does not have query parameters.
+// Even though bodies can be in many different formats, we will be picky
+// and require that it be a json object
+// {"vehicle_type":"goat", "cost":12, "size":"large"}
+document.color = req.body.color;
+document.model = req.body.model;
+document.year = req.body.year;
+try{
+let result = await document.save();
+res.send(result);
+}
+catch(err){
+res.status(500);
+res.send(`{"error": ${err}}`);
+}
+};
 
 // for a specific vehicle.
 exports.vehicle_detail = function(req, res) {
 res.send('NOT IMPLEMENTED: vehicle detail: ' + req.params.id);
 };
-// Handle vehicle create on POST.
-exports.vehicle_create_post = function(req, res) {
-res.send('NOT IMPLEMENTED: vehicle create POST');
-};
+
 // Handle vehicle delete form on DELETE.
 exports.vehicle_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: vehicle delete DELETE ' + req.params.id);
--- routes/vehicle.js
+++ routes/vehicle.js
@@ -1,9 +1,8 @@
+
 var express = require('express');
+const vehicle_controlers= require('../controllers/vehicle');
 var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('vehicle', { title: 'Search Result Vehicle' });
-});
-
+/* GET costumes */
+router.get('/', vehicle_controlers.vehicle_view_all_Page );
 module.exports = router;
+
