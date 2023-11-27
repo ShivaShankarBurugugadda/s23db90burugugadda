883e908341a83c178697b0116ac3e90c6f5f1858
ShivaShankarBurugugadda <143209402+ShivaShankarBurugugadda@users.noreply.github.com>
2023-11-26 19:37:57 -0600

    deploy commit

--- models/vehicle.js
+++ models/vehicle.js
@@ -1,8 +1,17 @@
 const mongoose = require("mongoose")
 const vehicleSchema = mongoose.Schema({
 color:String,
-model: String,
-year: Number,
+model: {
+    type: String,
+    minlength: 3,
+    maxlength: 50,
+  },
+year: {
+    type: Number,
+    required: true,
+    min: 1900, 
+    max: 2023, 
+  },
 
 })
 module.exports = mongoose.model("vehicle",
